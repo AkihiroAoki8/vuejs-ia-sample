faf78bc3dde260e5c5350436b96e4d07e34fefb8
AkihiroAoki8 <playfulthink@gmail.com>
2022-05-17 12:06:11 +0900

    vue-cli最初

---
+++ npm/vue-cli-test/src/App.vue
@@ -0,0 +1,55 @@
+<template>
+  <!-- templateタグでかこった範囲が仮想DOM -->
+  <div>
+    <img alt="Vue logo" src="./assets/logo.png">
+
+    <!-- step3. componentで指定したコンポーネントを
+    設定できる
+    -->
+    <!-- コンポーネント その中に属性をつけられる(props) -->
+    <HelloWorld msg="Vue3 start 123"/>
+
+    <!-- 子コンポーネントから渡ってきたイベント -->
+    <HelloWorld @customEvent="parentFunction"></Helloworld>
+  </div>
+</template>
+
+<script>
+// JS Vue.js
+// 他のコンポーネントをインポートする方法
+// step1. importで読み込む
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  
+  // step2. componentsを書いてその中に
+  // インポートしたコンポーネントの名前を書く
+  components: {
+    HelloWorld
+  },
+  data(){
+    return {
+      appTest: 'Appコンポーネントの値です'
+    }
+  },
+  methods: {
+    parentFunction(){
+      console.log('親の関数が実行されます')
+    }
+  },
+  created(){}
+}
+</script>
+
+<style>
+/* styleはcssをかく */
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ npm/vue-cli-test/src/components/HelloWorld.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <button @click="information">親に打ち上げる</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    // このコンポーネントが持ってるプロパティ
+    // 親から渡せる 受け皿
+    msg: String
+  },
+  data(){
+    return { 
+      helloworld: 'helloworldのデータです'
+    }
+  },
+  methods: {
+    information(){
+      // 子から親に打ち上げる
+      // 1つめの引数は カスタムイベントの名前
+      // 2つめの引数で渡したいデータ
+      this.$emit('customEvent', '子からデータを打ち上げました')
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
